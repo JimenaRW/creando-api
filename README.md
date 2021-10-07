a313340db664f19b61a107955435465252d18a48
jimenarw <jimena.witen@gmail.com>
2021-10-07 15:13:58 -0300

    estructura con crud de movies y genres

---
+++ README.md
@@ -0,0 +1,7 @@
+# Creación de una API.
+
+## Requerimientos:
+
++ Correr ~~ npm i ~~ para instalar paquetes de dependencias.
++ Configurar archivo ./src/database/config/config de base de datos, tomar de base el archivo .env.example
++ Correr servidor con comando ~~ npm run test ~~
---
+++ src/database/config/config.js
@@ -0,0 +1,28 @@
+require('dotenv').config()
+
+module.exports = {
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASS,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+    port: process.env.DB_PORT
+  },
+  test: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASS,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+    port: process.env.DB_PORT
+  },
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASS,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+    port: process.env.DB_PORT
+  }
+}
